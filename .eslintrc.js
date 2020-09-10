d5452308fb07e92c542f6eaffbac9f0b861bc0b3
Matt Greenwood <12268867+mjgwood@users.noreply.github.com>
2020-09-10 17:27:59 +0800

    lint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:vue/essential"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["vue"],
+  rules: {}
+};
