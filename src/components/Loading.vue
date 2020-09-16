470784684665755509870c3a27b2f16511cf2267
Matt Greenwood <12268867+mjgwood@users.noreply.github.com>
2020-09-16 13:46:24 +0800

    extract Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="loading">
+    <p>Loading your questions...</p>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      style="margin: auto; display: block;"
+      width="200px"
+      height="200px"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <rect x="17.5" y="30" width="15" height="40" fill="#dd1785">
+        <animate
+          attributeName="y"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="18;30;30"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.2s"
+        />
+        <animate
+          attributeName="height"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="64;40;40"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.2s"
+        />
+      </rect>
+      <rect x="42.5" y="30" width="15" height="40" fill="#ce2683">
+        <animate
+          attributeName="y"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="20.999999999999996;30;30"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.1s"
+        />
+        <animate
+          attributeName="height"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="58.00000000000001;40;40"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.1s"
+        />
+      </rect>
+      <rect x="67.5" y="30" width="15" height="40" fill="#b21c77">
+        <animate
+          attributeName="y"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="20.999999999999996;30;30"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+        />
+        <animate
+          attributeName="height"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="58.00000000000001;40;40"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+        />
+      </rect>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+};
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  p {
+    font-size: 2rem;
+  }
+}
+</style>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,82 +1,6 @@
 <template>
   <div>
-    <div class="loading" v-if="questions.length == 0">
-      <p>Loading your questions...</p>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        style="margin: auto; display: block;"
-        width="200px"
-        height="200px"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="xMidYMid"
-      >
-        <rect x="17.5" y="30" width="15" height="40" fill="#dd1785">
-          <animate
-            attributeName="y"
-            repeatCount="indefinite"
-            dur="1s"
-            calcMode="spline"
-            keyTimes="0;0.5;1"
-            values="18;30;30"
-            keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-            begin="-0.2s"
-          />
-          <animate
-            attributeName="height"
-            repeatCount="indefinite"
-            dur="1s"
-            calcMode="spline"
-            keyTimes="0;0.5;1"
-            values="64;40;40"
-            keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-            begin="-0.2s"
-          />
-        </rect>
-        <rect x="42.5" y="30" width="15" height="40" fill="#ce2683">
-          <animate
-            attributeName="y"
-            repeatCount="indefinite"
-            dur="1s"
-            calcMode="spline"
-            keyTimes="0;0.5;1"
-            values="20.999999999999996;30;30"
-            keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-            begin="-0.1s"
-          />
-          <animate
-            attributeName="height"
-            repeatCount="indefinite"
-            dur="1s"
-            calcMode="spline"
-            keyTimes="0;0.5;1"
-            values="58.00000000000001;40;40"
-            keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-            begin="-0.1s"
-          />
-        </rect>
-        <rect x="67.5" y="30" width="15" height="40" fill="#b21c77">
-          <animate
-            attributeName="y"
-            repeatCount="indefinite"
-            dur="1s"
-            calcMode="spline"
-            keyTimes="0;0.5;1"
-            values="20.999999999999996;30;30"
-            keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-          />
-          <animate
-            attributeName="height"
-            repeatCount="indefinite"
-            dur="1s"
-            calcMode="spline"
-            keyTimes="0;0.5;1"
-            values="58.00000000000001;40;40"
-            keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-          />
-        </rect>
-      </svg>
-    </div>
+    <Loading v-if="questions.length == 0" />
     <div
       class="quiz"
       v-else-if="currentQuestionIndex < questions.length"
@@ -157,13 +81,18 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+import Loading from './Loading.vue';
+
 const questions = [],
   chosenAnswers = [];
 
-import axios from 'axios';
-
 export default {
   name: 'Quiz',
+  components: {
+    Loading,
+  },
   data() {
     return {
       questions: questions,
@@ -367,11 +296,6 @@ a {
   &[disabled] {
     background-color: #a9aaac;
     pointer-events: none;
-  }
-}
-.loading {
-  p {
-    font-size: 2rem;
   }
 }
 .answers-container {
